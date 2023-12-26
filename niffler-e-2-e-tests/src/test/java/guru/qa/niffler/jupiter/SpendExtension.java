565b9f6ec55a8c515e893019afc2bd962267bae2
VTB4065719@region.vtb.ru <evT6TAb33$Ssq7Qq>
2023-12-26 23:48:26 +0300

    HomeWork_1

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/SpendExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/SpendExtension.java
@@ -1,6 +1,7 @@
 package guru.qa.niffler.jupiter;
 
 import guru.qa.niffler.api.SpendApi;
+import guru.qa.niffler.model.CategoryJson;
 import guru.qa.niffler.model.SpendJson;
 import okhttp3.OkHttpClient;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
@@ -17,53 +18,54 @@ import java.util.Optional;
 
 public class SpendExtension implements BeforeEachCallback, ParameterResolver {
 
-  public static final ExtensionContext.Namespace NAMESPACE
-      = ExtensionContext.Namespace.create(SpendExtension.class);
+    public static final ExtensionContext.Namespace NAMESPACE
+            = ExtensionContext.Namespace.create(SpendExtension.class);
 
-  private static final OkHttpClient httpClient = new OkHttpClient.Builder().build();
-  private static final Retrofit retrofit = new Retrofit.Builder()
-      .client(httpClient)
-      .baseUrl("http://127.0.0.1:8093")
-      .addConverterFactory(JacksonConverterFactory.create())
-      .build();
+    private static final OkHttpClient httpClient = new OkHttpClient.Builder().build();
+    private static final Retrofit retrofit = new Retrofit.Builder()
+            .client(httpClient)
+            .baseUrl("http://127.0.0.1:8093")
+            .addConverterFactory(JacksonConverterFactory.create())
+            .build();
 
-  private final SpendApi spendApi = retrofit.create(SpendApi.class);
+    private final SpendApi spendApi = retrofit.create(SpendApi.class);
 
-  @Override
-  public void beforeEach(ExtensionContext extensionContext) throws Exception {
-    Optional<GenerateSpend> spend = AnnotationSupport.findAnnotation(
-        extensionContext.getRequiredTestMethod(),
-        GenerateSpend.class
-    );
+    @Override
+    public void beforeEach(ExtensionContext extensionContext) throws Exception {
+        Optional<GenerateSpend> spend = AnnotationSupport.findAnnotation(
+                extensionContext.getRequiredTestMethod(),
+                GenerateSpend.class
+        );
 
-    if (spend.isPresent()) {
-      GenerateSpend spendData = spend.get();
-      SpendJson spendJson = new SpendJson(
-          null,
-          new Date(),
-          spendData.category(),
-          spendData.currency(),
-          spendData.amount(),
-          spendData.description(),
-          spendData.username()
-      );
+        if (spend.isPresent()) {
+            GenerateSpend spendData = spend.get();
+            SpendJson spendJson = new SpendJson(
+                    null,
+                    new Date(),
+                    extensionContext.getStore(CategoryExtension.NAMESPACE)
+                            .get(CategoryExtension.MAP_KEY, CategoryJson.class).category(),
+                    spendData.currency(),
+                    spendData.amount(),
+                    spendData.description(),
+                    spendData.username()
+            );
 
-      SpendJson created = spendApi.addSpend(spendJson).execute().body();
-      extensionContext.getStore(NAMESPACE)
-          .put("spend", created);
+            SpendJson created = spendApi.addSpend(spendJson).execute().body();
+            extensionContext.getStore(NAMESPACE)
+                    .put("spend", created);
+        }
     }
-  }
 
-  @Override
-  public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
-    return parameterContext.getParameter()
-        .getType()
-        .isAssignableFrom(SpendJson.class);
-  }
+    @Override
+    public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        return parameterContext.getParameter()
+                .getType()
+                .isAssignableFrom(SpendJson.class);
+    }
 
-  @Override
-  public SpendJson resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
-    return extensionContext.getStore(NAMESPACE)
-        .get("spend", SpendJson.class);
-  }
+    @Override
+    public SpendJson resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
+        return extensionContext.getStore(NAMESPACE)
+                .get("spend", SpendJson.class);
+    }
 }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/SpendingTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/SpendingTest.java
@@ -1,54 +1,35 @@
 package guru.qa.niffler.test;
 
-import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import guru.qa.niffler.jupiter.GenerateCategory;
 import guru.qa.niffler.jupiter.GenerateSpend;
 import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
-import org.junit.jupiter.api.BeforeEach;
+import guru.qa.niffler.pages.WelcomePage;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.CollectionCondition.size;
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
 
 public class SpendingTest {
 
-  static {
-    Configuration.browserSize = "1980x1024";
-  }
+    private final String userName = "duck";
 
-  @BeforeEach
-  void doLogin() {
-    Selenide.open("http://127.0.0.1:3000/main");
-    $("a[href*='redirect']").click();
-    $("input[name='username']").setValue("duck");
-    $("input[name='password']").setValue("12345");
-    $("button[type='submit']").click();
-  }
-
-  @GenerateSpend(
-      username = "duck",
-      description = "QA.GURU Advanced 4",
-      amount = 72500.00,
-      category = "Обучение",
-      currency = CurrencyValues.RUB
-  )
-  @Test
-  void spendingShouldBeDeletedByButtonDeleteSpending(SpendJson spend) {
-    $(".spendings-table tbody")
-        .$$("tr")
-        .find(text(spend.description()))
-        .$$("td")
-        .first()
-        .click();
-
-    $(byText("Delete selected"))
-        .click();
-
-    $(".spendings-table tbody")
-        .$$("tr")
-        .shouldHave(size(0));
-  }
+    @GenerateCategory(
+            username = userName,
+            category = "укуукук"
+    )
+    @GenerateSpend(
+            username = userName,
+            description = "Белый кролик",
+            amount = 96500.00,
+            currency = CurrencyValues.RUB
+    )
+    @Test
+    void spendingShouldBeDeletedByButtonDeleteSpending(SpendJson spend) {
+        Selenide.open("http://127.0.0.1:3000/main", WelcomePage.class)
+                .clickLoginButton()
+                .authorize(userName, "12345")
+                .selectRowInTableByText(spend.category())
+                .clickDeleteButton()
+                .checkTableIsEmpty();
+    }
 }
