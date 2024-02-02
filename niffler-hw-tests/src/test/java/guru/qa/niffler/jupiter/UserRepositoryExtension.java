0dc5468e0bdab3b5a9d413484563a7cc2f016c33
Tester <tester@test.com>
2024-02-02 19:05:40 +0200

    Add UserRepository implementation selection from -D parameter.

--- niffler-hw-tests/src/test/java/guru/qa/niffler/jupiter/UserRepositoryExtension.java
+++ niffler-hw-tests/src/test/java/guru/qa/niffler/jupiter/UserRepositoryExtension.java
@@ -2,19 +2,31 @@ package guru.qa.niffler.jupiter;
 
 import guru.qa.niffler.db.repository.UserRepository;
 import guru.qa.niffler.db.repository.UserRepositoryJdbc;
+import guru.qa.niffler.db.repository.UserRepositorySJdbc;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestInstancePostProcessor;
 
 import java.lang.reflect.Field;
 
 public class UserRepositoryExtension implements TestInstancePostProcessor {
-  @Override
-  public void postProcessTestInstance(Object o, ExtensionContext extensionContext) throws Exception {
-    for (Field field : o.getClass().getDeclaredFields()) {
-      if (field.getType().isAssignableFrom(UserRepository.class)) {
-        field.setAccessible(true);
-        field.set(o, new UserRepositoryJdbc());
-      }
+    @Override
+    public void postProcessTestInstance(Object o, ExtensionContext extensionContext) throws Exception {
+        for (Field field : o.getClass().getDeclaredFields()) {
+            if (field.getType().isAssignableFrom(UserRepository.class)) {
+                field.setAccessible(true);
+                String paramValue = System.getProperty("repository");
+
+                switch (paramValue) {
+                    case "jdbc":
+                        field.set(o, new UserRepositoryJdbc());
+                        break;
+                    case "sjdbc":
+                        field.set(o, new UserRepositorySJdbc());
+                        break;
+                    default:
+                        throw new RuntimeException("Incorrect repository in parameter.");
+                }
+            }
+        }
     }
-  }
 }
