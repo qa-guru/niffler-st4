db5803bc99de8bf3ed48a46e1ce2fc54a9c9bc3e
VTB4065719@region.vtb.ru <evT6TAb33$Ssq7Qq>
2024-02-05 15:30:00 +0300

    add hw8
    Signed-off-by: VTB4065719@region.vtb.ru <evT6TAb33$Ssq7Qq>

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/db/repository/UserRepositorySJdbc.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/db/repository/UserRepositorySJdbc.java
@@ -2,6 +2,8 @@ package guru.qa.niffler.db.repository;
 
 import guru.qa.niffler.db.DataSourceProvider;
 import guru.qa.niffler.db.JdbcUrl;
+import guru.qa.niffler.db.model.Authority;
+import guru.qa.niffler.db.model.AuthorityEntity;
 import guru.qa.niffler.db.model.UserAuthEntity;
 import guru.qa.niffler.db.model.UserEntity;
 import guru.qa.niffler.db.sjdbc.UserAuthEntityResultSetExtractor;
@@ -18,139 +20,198 @@ import org.springframework.transaction.support.TransactionTemplate;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
 
 public class UserRepositorySJdbc implements UserRepository {
 
-  private final TransactionTemplate authTxt;
-  private final TransactionTemplate udTxt;
-  private final JdbcTemplate authTemplate;
-  private final JdbcTemplate udTemplate;
+    private final TransactionTemplate authTxt;
+    private final TransactionTemplate udTxt;
+    private final JdbcTemplate authTemplate;
+    private final JdbcTemplate udTemplate;
 
-  private final PasswordEncoder pe = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    private final PasswordEncoder pe = PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
-  public UserRepositorySJdbc() {
-    JdbcTransactionManager authTm = new JdbcTransactionManager(
-        DataSourceProvider.INSTANCE.dataSource(JdbcUrl.AUTH)
-    );
-    JdbcTransactionManager udTm = new JdbcTransactionManager(
-        DataSourceProvider.INSTANCE.dataSource(JdbcUrl.USERDATA)
-    );
-
-    this.authTxt = new TransactionTemplate(authTm);
-    this.udTxt = new TransactionTemplate(udTm);
-    this.authTemplate = new JdbcTemplate(authTm.getDataSource());
-    this.udTemplate = new JdbcTemplate(udTm.getDataSource());
-  }
-
-  @Override
-  public UserAuthEntity createInAuth(UserAuthEntity user) {
-    KeyHolder kh = new GeneratedKeyHolder();
-    return authTxt.execute(status -> {
-      authTemplate.update(con -> {
-        PreparedStatement ps = con.prepareStatement(
-            "INSERT INTO \"user\" " +
-                "(username, password, enabled, account_non_expired, account_non_locked, credentials_non_expired) " +
-                "VALUES (?, ?, ?, ?, ?, ?)",
-            PreparedStatement.RETURN_GENERATED_KEYS
+    public UserRepositorySJdbc() {
+        JdbcTransactionManager authTm = new JdbcTransactionManager(
+                DataSourceProvider.INSTANCE.dataSource(JdbcUrl.AUTH)
         );
-        ps.setString(1, user.getUsername());
-        ps.setString(2, pe.encode(user.getPassword()));
-        ps.setBoolean(3, user.getEnabled());
-        ps.setBoolean(4, user.getAccountNonExpired());
-        ps.setBoolean(5, user.getAccountNonLocked());
-        ps.setBoolean(6, user.getCredentialsNonExpired());
-        return ps;
-      }, kh);
+        JdbcTransactionManager udTm = new JdbcTransactionManager(
+                DataSourceProvider.INSTANCE.dataSource(JdbcUrl.USERDATA)
+        );
 
-      user.setId((UUID) kh.getKeys().get("id"));
-
-      authTemplate.batchUpdate("INSERT INTO \"authority\" " +
-          "(user_id, authority) " +
-          "VALUES (?, ?)", new BatchPreparedStatementSetter() {
-        @Override
-        public void setValues(PreparedStatement ps, int i) throws SQLException {
-          ps.setObject(1, user.getId());
-          ps.setString(2, user.getAuthorities().get(i).getAuthority().name());
-        }
-
-        @Override
-        public int getBatchSize() {
-          return user.getAuthorities().size();
-        }
-      });
-
-      return user;
-    });
-  }
-
-  @Override
-  public Optional<UserAuthEntity> findByIdInAuth(UUID id) {
-    try {
-      return Optional.ofNullable(
-          authTemplate.query(
-              "SELECT * " +
-                  "FROM \"user\" u " +
-                  "JOIN \"authority\" a ON u.id = a.user_id " +
-                  "where u.id = ?",
-              UserAuthEntityResultSetExtractor.instance,
-              id
-          )
-      );
-    } catch (EmptyResultDataAccessException e) {
-      return Optional.empty();
+        this.authTxt = new TransactionTemplate(authTm);
+        this.udTxt = new TransactionTemplate(udTm);
+        this.authTemplate = new JdbcTemplate(authTm.getDataSource());
+        this.udTemplate = new JdbcTemplate(udTm.getDataSource());
     }
-  }
 
-  @Override
-  public UserEntity createInUserdata(UserEntity user) {
-    KeyHolder kh = new GeneratedKeyHolder();
-    udTemplate.update(con -> {
-      PreparedStatement ps = con.prepareStatement(
-          "INSERT INTO \"user\" (username, currency) VALUES (?, ?)",
-          PreparedStatement.RETURN_GENERATED_KEYS
-      );
-      ps.setString(1, user.getUsername());
-      ps.setString(2, user.getCurrency().name());
-      return ps;
-    }, kh);
+    @Override
+    public UserAuthEntity createInAuth(UserAuthEntity user) {
+        KeyHolder kh = new GeneratedKeyHolder();
+        return authTxt.execute(status -> {
+            authTemplate.update(con -> {
+                PreparedStatement ps = con.prepareStatement(
+                        "INSERT INTO \"user\" " +
+                                "(username, password, enabled, account_non_expired, account_non_locked, credentials_non_expired) " +
+                                "VALUES (?, ?, ?, ?, ?, ?)",
+                        PreparedStatement.RETURN_GENERATED_KEYS
+                );
+                ps.setString(1, user.getUsername());
+                ps.setString(2, pe.encode(user.getPassword()));
+                ps.setBoolean(3, user.getEnabled());
+                ps.setBoolean(4, user.getAccountNonExpired());
+                ps.setBoolean(5, user.getAccountNonLocked());
+                ps.setBoolean(6, user.getCredentialsNonExpired());
+                return ps;
+            }, kh);
 
-    user.setId((UUID) kh.getKeys().get("id"));
-    return user;
-  }
+            user.setId((UUID) kh.getKeys().get("id"));
 
-  @Override
-  public Optional<UserEntity> findByIdInUserdata(UUID id) {
-    try {
-      return Optional.ofNullable(
-          udTemplate.queryForObject(
-              "SELECT * FROM \"user\" WHERE id = ?",
-              UserEntityRowMapper.instance,
-              id
-          )
-      );
-    } catch (EmptyResultDataAccessException e) {
-      return Optional.empty();
+            authTemplate.batchUpdate("INSERT INTO \"authority\" " +
+                    "(user_id, authority) " +
+                    "VALUES (?, ?)", new BatchPreparedStatementSetter() {
+                @Override
+                public void setValues(PreparedStatement ps, int i) throws SQLException {
+                    ps.setObject(1, user.getId());
+                    ps.setString(2, user.getAuthorities().get(i).getAuthority().name());
+                }
+
+                @Override
+                public int getBatchSize() {
+                    return user.getAuthorities().size();
+                }
+            });
+
+            return user;
+        });
     }
-  }
 
-  @Override
-  public void deleteInAuthById(UUID id) {
-    authTxt.execute(status -> {
-      authTemplate.update("DELETE FROM \"authority\" WHERE user_id = ?", id);
-      authTemplate.update("DELETE FROM \"user\" WHERE id = ?", id);
-      return null;
-    });
-  }
+    @Override
+    public Optional<UserAuthEntity> findByIdInAuth(UUID id) {
+        try {
+            return Optional.ofNullable(
+                    authTemplate.query(
+                            "SELECT * " +
+                                    "FROM \"user\" u " +
+                                    "JOIN \"authority\" a ON u.id = a.user_id " +
+                                    "where u.id = ?",
+                            UserAuthEntityResultSetExtractor.instance,
+                            id
+                    )
+            );
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
 
-  @Override
-  public void deleteInUserdataById(UUID id) {
-    udTxt.execute(status -> {
-      udTemplate.update("DELETE FROM friendship WHERE friend_id = ?", id);
-      udTemplate.update("DELETE FROM friendship WHERE user_id = ?", id);
-      udTemplate.update("DELETE FROM \"user\" WHERE id = ?", id);
-      return null;
-    });
-  }
+    @Override
+    public UserEntity createInUserdata(UserEntity user) {
+        KeyHolder kh = new GeneratedKeyHolder();
+        udTemplate.update(con -> {
+            PreparedStatement ps = con.prepareStatement(
+                    "INSERT INTO \"user\" (username, currency) VALUES (?, ?)",
+                    PreparedStatement.RETURN_GENERATED_KEYS
+            );
+            ps.setString(1, user.getUsername());
+            ps.setString(2, user.getCurrency().name());
+            return ps;
+        }, kh);
+
+        user.setId((UUID) kh.getKeys().get("id"));
+        return user;
+    }
+
+    @Override
+    public Optional<UserEntity> findByIdInUserdata(UUID id) {
+        try {
+            return Optional.ofNullable(
+                    udTemplate.queryForObject(
+                            "SELECT * FROM \"user\" WHERE id = ?",
+                            UserEntityRowMapper.instance,
+                            id
+                    )
+            );
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public void deleteInAuthById(UUID id) {
+        authTxt.execute(status -> {
+            authTemplate.update("DELETE FROM \"authority\" WHERE user_id = ?", id);
+            authTemplate.update("DELETE FROM \"user\" WHERE id = ?", id);
+            return null;
+        });
+    }
+
+    @Override
+    public void deleteInUserdataById(UUID id) {
+        udTxt.execute(status -> {
+            udTemplate.update("DELETE FROM friendship WHERE friend_id = ?", id);
+            udTemplate.update("DELETE FROM friendship WHERE user_id = ?", id);
+            udTemplate.update("DELETE FROM \"user\" WHERE id = ?", id);
+            return null;
+        });
+    }
+
+    @Override
+    public Optional<UserAuthEntity> updateUserInAuth(UserAuthEntity user) {
+        return authTxt.execute(status -> {
+            authTemplate.update(con -> {
+                PreparedStatement ps = con.prepareStatement(
+                        "UPDATE \"user\" " +
+                                "SET username=?, password=?, enabled=?, account_non_expired=?, account_non_locked=?, credentials_non_expired=? " +
+                                "WHERE id=CAST(? AS UUID)"
+                );
+                ps.setString(1, user.getUsername());
+                ps.setString(2, pe.encode(user.getPassword()));
+                ps.setBoolean(3, user.getEnabled());
+                ps.setBoolean(4, user.getAccountNonExpired());
+                ps.setBoolean(5, user.getAccountNonLocked());
+                ps.setBoolean(6, user.getCredentialsNonExpired());
+                ps.setString(7, user.getId().toString());
+                return ps;
+            });
+
+            authTemplate.update("DELETE FROM \"authority\" WHERE user_id= ? ", user.getId());
+
+            authTemplate.batchUpdate("INSERT INTO \"authority\" " +
+                    "(user_id, authority) " +
+                    "VALUES (?, ?)", new BatchPreparedStatementSetter() {
+                @Override
+                public void setValues(PreparedStatement ps, int i) throws SQLException {
+                    ps.setObject(1, user.getId());
+                    ps.setString(2, user.getAuthorities().get(i).getAuthority().name());
+                }
+
+                @Override
+                public int getBatchSize() {
+                    return user.getAuthorities().size();
+                }
+            });
+
+            return Optional.of(user);
+        });
+    }
+
+    @Override
+    public UserEntity updateUserInData(UserEntity user) {
+        udTemplate.update("""
+                        UPDATE "user"
+                        SET username = ?,
+                            currency = ?,
+                            firstname = ?,
+                            surname = ?,
+                            photo = ?
+                        WHERE id = ?
+                         """, user.getUsername(),
+                user.getCurrency().name(),
+                user.getFirstname(),
+                user.getSurname(),
+                user.getPhoto(),
+                user.getId());
+        return user;
+    }
+
 }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/UserRepositoryExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/UserRepositoryExtension.java
@@ -2,6 +2,7 @@ package guru.qa.niffler.jupiter;
 
 import guru.qa.niffler.db.repository.UserRepository;
 import guru.qa.niffler.db.repository.UserRepositoryJdbc;
+import guru.qa.niffler.db.repository.UserRepositorySJdbc;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestInstancePostProcessor;
 
@@ -13,7 +14,7 @@ public class UserRepositoryExtension implements TestInstancePostProcessor {
     for (Field field : o.getClass().getDeclaredFields()) {
       if (field.getType().isAssignableFrom(UserRepository.class)) {
         field.setAccessible(true);
-        field.set(o, new UserRepositoryJdbc());
+        field.set(o, System.getProperty("repository").equals("jdbc") ? new UserRepositoryJdbc() : new UserRepositorySJdbc());
       }
     }
   }
