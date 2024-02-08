a469d1edf9ce2a6b8710a51dc3020ffe06c5d491
VTB4065719@region.vtb.ru <evT6TAb33$Ssq7Qq>
2024-02-08 15:54:18 +0300

    fix
    Signed-off-by: VTB4065719@region.vtb.ru <evT6TAb33$Ssq7Qq>

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/UserRepositoryExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/UserRepositoryExtension.java
@@ -9,13 +9,13 @@ import org.junit.jupiter.api.extension.TestInstancePostProcessor;
 import java.lang.reflect.Field;
 
 public class UserRepositoryExtension implements TestInstancePostProcessor {
-  @Override
-  public void postProcessTestInstance(Object o, ExtensionContext extensionContext) throws Exception {
-    for (Field field : o.getClass().getDeclaredFields()) {
-      if (field.getType().isAssignableFrom(UserRepository.class)) {
-        field.setAccessible(true);
-        field.set(o, System.getProperty("repository").equals("jdbc") ? new UserRepositoryJdbc() : new UserRepositorySJdbc());
-      }
+    @Override
+    public void postProcessTestInstance(Object o, ExtensionContext extensionContext) throws Exception {
+        for (Field field : o.getClass().getDeclaredFields()) {
+            if (field.getType().isAssignableFrom(UserRepository.class)) {
+                field.setAccessible(true);
+                field.set(o, ("jdbc").equals(System.getProperty("repository")) ? new UserRepositoryJdbc() : new UserRepositorySJdbc());
+            }
+        }
     }
-  }
 }
